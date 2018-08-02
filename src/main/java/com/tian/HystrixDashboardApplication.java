4e5dccd9a36ab46320e6faba6d0154239922ecec
t912772863 <912772863@qq.com>
2018-08-02 10:33:11 +0800

    Initial commit

---
+++ src/main/java/com/tian/HystrixDashboardApplication.java
@@ -0,0 +1,18 @@
+package com.tian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @EnableHystrixDashboard 注解可以开启, Hystrix的面板监控功能
+ *
+ */
+@EnableHystrixDashboard
+@SpringBootApplication
+public class HystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDashboardApplication.class, args);
+	}
+}
